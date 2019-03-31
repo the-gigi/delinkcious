104c9c32ce65670263682a8195f5c98f342fc0be
Gigi <gigi.sayfan@helix.com>
2019-03-31 12:24:20 -0700

    Add link checker + test

---
+++ pkg/link_manager/link_checker.go
@@ -0,0 +1,18 @@
+package link_manager
+
+import (
+	"errors"
+	"net/http"
+)
+
+// CheckLinks tries to get the headers of the target url and returns error if it fails
+func CheckLink(url string) (err error) {
+	resp, err := http.Head(url)
+	if err != nil {
+		return
+	}
+	if resp.StatusCode >= 400 {
+		err = errors.New(resp.Status)
+	}
+	return
+}
---
+++ pkg/link_manager/link_checker_test.go
@@ -0,0 +1,18 @@
+package link_manager
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("Link checker tests", func() {
+	It("should not return error for a valid url", func() {
+		err := CheckLink("https://github.com/the-gigi/delinkcious")
+		Ω(err).Should(BeNil())
+	})
+
+	It("should not return error for non-existent url", func() {
+		err := CheckLink("https://github.com/the-gigi/delinkcious/no-such-url")
+		Ω(err).ShouldNot(BeNil())
+	})
+})
