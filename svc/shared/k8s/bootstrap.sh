2e53c3b0eac54f12e60ac784c24de6799e9ffc72
Gigi <gigi.sayfan@helix.com>
2019-05-13 22:52:45 -0700

    Progress on chapter 12. Done with logging

--- svc/shared/k8s/bootstrap.sh
+++ svc/shared/k8s/bootstrap.sh
@@ -1,20 +1,43 @@
 #!/bin/bash
 set -euxo pipefail
 
+
+###############################
+#
+#   E N A B L E   A D D O N S
+#
+###############################
+#minikube addons enable ingress
+#minikube addons enable heapster
+#minikube addons enable efk
+#minikube addons enable metrics-server
+
+###############################
+#
+#   I N S T A L L   N A T S
+#
+###############################
+#kubectl apply -f https://github.com/nats-io/nats-operator/releases/download/v0.4.5/00-prereqs.yaml
+#kubectl apply -f https://github.com/nats-io/nats-operator/releases/download/v0.4.5/10-deployment.yaml
+#kubectl create -f nats_cluster.yaml
+
+
 ###############################
 #
 #   I N S T A L L   A E G O C D
 #
 ###############################
-kubectl create namespace argocd
-kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+#kubectl create namespace argocd
+#kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+
+#sleep 3000
 
 # Initial password is the ArgoCD server pod id
 TEMP_PASSWORD=$(kubectl get pods -n argocd -l app.kubernetes.io/name=argocd-server -o name | cut -d'/' -f 2)
 
 # Port-forward to access the Argo CD server locally on port 8080:
 kubectl port-forward -n argocd svc/argocd-server 8080:443 &
-sleep 3
+sleep 3000
 
 # Login and update password
 argocd login :8080 --insecure --username admin --password $TEMP_PASSWORD
@@ -23,12 +46,3 @@ argocd account update-password --current-password $TEMP_PASSWORD --new-password 
 # Add Delinkcious services
 python bootstrap_argocd.py
 
-###############################
-#
-#   I N S T A L L   N A T S
-#
-###############################
-kubectl apply -f https://github.com/nats-io/nats-operator/releases/download/v0.4.5/00-prereqs.yaml
-kubectl apply -f https://github.com/nats-io/nats-operator/releases/download/v0.4.5/10-deployment.yaml
-
-kubectl create -f nats_cluster.yaml
