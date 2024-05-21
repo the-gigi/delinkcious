5ed0202f37cf7dc8e2f4b33fa37cabcacf3e6930
the-gigi <the.gigi@gmail.com>
2024-05-20 20:53:21 -0700

    build images for both amd64 and arm64

--- svc/shared/docker/troubleshooter/build.sh
+++ svc/shared/docker/troubleshooter/build.sh
@@ -2,5 +2,15 @@ IMAGE=g1g1/py-kube:0.4
 
 echo $DOCKERHUB_PASSWORD | docker login -u $DOCKERHUB_USERNAME --password-stdin
 
-docker build . -t $IMAGE
-docker push $IMAGE
+# Create and use a new builder instance
+docker buildx create --use
+
+# Build and push the amd64 image
+docker build --platform linux/amd64 \
+    --build-arg ARCH=amd64 \
+    -t ${IMAGE}-amd64 --push .
+
+# Build and push the arm64 image
+docker build --platform linux/arm64 \
+    --build-arg ARCH=arm64 \
+    -t ${IMAGE}-arm64 --push .
